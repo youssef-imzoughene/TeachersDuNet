0442313d908d3e7702c196bec606032b6a05e3df
imzoughene <imzoughene.youssef.01@gmail.com>
2019-01-21 17:41:07 +0100

    continue at Home

--- blog/resources/views/events/index.blade.php
+++ blog/resources/views/events/index.blade.php
@@ -1,54 +1,28 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-                padding: 5%;
-            }
-
-
-        </style>
-    </head>
-    <body>
-        <h1>Events</h1>
-        <p>
-            @php
-                //var_dump($events);
-                //dump($events);
-            @endphp
-        </p>
-        //ancienne methode
-        <ul>
-            @php
-                foreach($events as $event):
-            @endphp
-            <li>
-                @php
-                    echo $event;
-                @endphp
-            </li>
-            @php
-                endforeach;
-            @endphp
-        </ul>
-        //nouveau methode blade
-        @foreach($events as $event)
-            <li> {{ $event }} </li>
-        @endforeach
-    </body>
-</html>
+@extends('layouts/master',['title'=>"devcorp"])
+@section('content')
+<h1>Events</h1>
+<p>
+    @php
+        //var_dump($events);
+        //dump($events);
+    @endphp
+</p>
+//ancienne methode
+<ul>
+    @php
+        foreach($events as $event):
+    @endphp
+    <li>
+        @php
+            echo $event;
+        @endphp
+    </li>
+    @php
+        endforeach;
+    @endphp
+</ul>
+//nouveau methode blade
+@foreach($events as $event)
+    <li> {{ $event }} </li>
+@endforeach
+@endsection
--- blog/resources/views/pages/about.blade.php
+++ blog/resources/views/pages/about.blade.php
@@ -1,34 +1,9 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-         
-        </style>
-    </head>
-    <body>
-        <h1>about us</h1>
-        <h2>my name is
-        @php
-            echo $name
-        @endphp 
-        </h2>
-    </body>
-</html>
+@extends("layouts.master")
+@section('content')
+<h1>about us</h1>
+<h2>my name is
+@php
+    echo $name
+@endphp
+</h2>
+@endsection
